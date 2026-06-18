4222aa4ff3967ffcce76e8673acefc214811d3d2
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:36:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
 app.get("/", (req, res) => {
-  res.json({
-    message: "E-Commerce API Running"
-  });
+  res.send("E-Commerce API Running");
 });
 
-const PORT = 5000;
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
