916547415dcd36e63bab75bccd62b81a00854aeb
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:43:55 +0530

    Update ProductCard.js

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,1 +1,36 @@
+import { useContext } from "react";
 
+import { CartContext }
+from "../context/CartContext";
+
+function ProductCard({ product }) {
+
+  const { addToCart } =
+    useContext(CartContext);
+
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+        margin: "10px",
+        padding: "10px"
+      }}
+    >
+      <h3>{product.name}</h3>
+
+      <p>{product.description}</p>
+
+      <p>₹{product.price}</p>
+
+      <button
+        onClick={() =>
+          addToCart(product)
+        }
+      >
+        Add To Cart
+      </button>
+    </div>
+  );
+}
+
+export default ProductCard;
