196fb8f77c95415711db90e4f93027b0fed599ac
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:44:58 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
+import {
+  AuthProvider
+} from "./context/AuthContext";
+
+import {
+  CartProvider
+} from "./context/CartContext";
+
+const root =
+ReactDOM.createRoot(
+document.getElementById("root")
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </AuthProvider>
 );
