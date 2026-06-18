cac1c2f638534313b3cef687a1d1e9b65e8d3cb9
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:35:38 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,1 +1,123 @@
+const express = require("express");
 
+const Product = require("../models/Product");
+const authMiddleware = require("../middleware/authMiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
+
+const router = express.Router();
+
+/* Get All Products */
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.json(products);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+/* Get Single Product */
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(
+      req.params.id
+    );
+
+    res.json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+/* Add Product (Admin Only) */
+
+router.post(
+  "/",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+
+    try {
+
+      const product =
+        await Product.create(req.body);
+
+      res.status(201).json(product);
+
+    } catch (error) {
+
+      res.status(500).json({
+        message: error.message
+      });
+
+    }
+  }
+);
+
+/* Update Product */
+
+router.put(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+
+    try {
+
+      const product =
+        await Product.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          { new: true }
+        );
+
+      res.json(product);
+
+    } catch (error) {
+
+      res.status(500).json({
+        message: error.message
+      });
+
+    }
+  }
+);
+
+/* Delete Product */
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+
+    try {
+
+      await Product.findByIdAndDelete(
+        req.params.id
+      );
+
+      res.json({
+        message: "Product Deleted"
+      });
+
+    } catch (error) {
+
+      res.status(500).json({
+        message: error.message
+      });
+
+    }
+  }
+);
+
+module.exports = router;
