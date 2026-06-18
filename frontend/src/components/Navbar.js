9e91554e963492efc97ad8409aa69f409ca6a94e
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:43:30 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,17 @@
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>{" | "}
+
+      <Link to="/cart">Cart</Link>{" | "}
+
+      <Link to="/orders">Orders</Link>{" | "}
+
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
