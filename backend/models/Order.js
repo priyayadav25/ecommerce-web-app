44b5d762b19c3809a36fd2f9f8b8fdb3dbf7e7dd
priyayadav25 <priyay2519@gmail.com>
2026-06-18 20:31:51 +0530

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,1 +1,33 @@
+const mongoose = require("mongoose");
 
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product"
+        },
+
+        quantity: Number
+      }
+    ],
+
+    totalAmount: Number,
+
+    status: {
+      type: String,
+      default: "Pending"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
